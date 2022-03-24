4368d62c9b1a4b86bde4026de08969b0b6955669
Season <64575267+MLCluanchar@users.noreply.github.com>
2022-03-24 12:46:09 +1030

    Update Zeus swap.lua

--- Zeus swap.lua
+++ Zeus swap.lua
@@ -1,4 +1,16 @@
+local configure_combobox = ui.new_combobox( "Rage", "Other", "Swap Zues for:",  
+"Auto",
+"Primary",
+"Secondary",
+"Knife")
 
+local function owo()
+    local ouo = ui.get(configure_combobox)
+    if ouo == "Auto" then return "slot2;slot1" end
+    if ouo == "Primary" then return "slot1" end
+    if ouo == "Secondary" then return "slot2" end
+    if ouo == "Knife" then return "slot3" end
+end
 client.set_event_callback('aim_fire', function()
     --Check if you are holding zeus
     local local_player = entity.get_local_player()
@@ -6,5 +18,5 @@ client.set_event_callback('aim_fire', function()
     local itemindex = bit.band(entity.get_prop(weapon, "m_iItemDefinitionIndex"), 0xFFFF)
     if itemindex ~= 31 then return end
     --Swap weapon after zeus fire
-    client.delay_call( 0.05, client.exec, "slot1")
+    client.exec(owo())
 end)
